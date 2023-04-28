a64f8ada30cdfcf2d6e6f60e6479d262d4bdf73a
Alicja5157 <129370388+Alicja5157@users.noreply.github.com>
2023-04-28 09:30:59 +0200

    Delete lo2.sh

--- lo2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#tworzy konkretne logi
-if [ "$1" = "--logs" ]
-	then
-	for i in $(seq 1 $2)
-	do
-	echo "Nazwa pliku to log$i.txt i został utworzony przez $0 dokładnie $(date)" > log$i.txt
-	done
-fi
